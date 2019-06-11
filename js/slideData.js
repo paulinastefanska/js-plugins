58578a07b2e4f0586c2891aad000534d3902eee7
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-06-11 12:08:14 +0200

    Move slideData to new js file

---
+++ js/slideData.js
@@ -0,0 +1,34 @@
+"use strict";
+
+var slideData = [
+	        {
+	          image: "images/seychelles.jpg",
+	          title: "Seychelles",
+	          description: "<p>beautiful beaches and a wide variety of marine life</p>",
+	          coords: {lat: -4.662014, lng: 55.470237}
+	        },
+	        {
+	          image: "images/philippines.jpg",
+	          title: "Philippines",
+	          description: "<p>wonderful wild nature with waterfalls</p>",
+	          coords: {lat: 12.361393, lng: 122.595898}
+	        },
+	        {
+	          image: "images/french-polynesia.jpg",
+	          title: "French Polynesia",
+	          description: "<p>luxury, beauty and turquoise water</p>",
+	          coords: {lat: -17.662631, lng: -149.448499}
+	        },
+	        {
+	          image: "images/singapore.jpg",
+	          title: "Singapore",
+	          description: "<p>the most beautiful garden in the city, interesting culture</p>",
+	          coords: {lat: 1.354496, lng: 103.866227}
+	        },
+	        {
+	          image: "images/australia.jpg",
+	          title: "Australia",
+	          description: "<p>greenery, kangaroos and wonderful beaches</p>",
+	          coords: {lat: -25.036283, lng: 134.338180}
+	        },
+	      ]
